ac9cbd9664bd8d5d582ba3d1edd7773f9fdc64d5
liujl25 <liujl25@163.com>
2022-05-04 17:23:33 +0800

    1.创建工程引入gateway网关，并简单测试

---
+++ gulimall-gateway/src/main/java/com/liujl/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.liujl.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1.开启服务注册发现
+ * （1）nacos注册中心地址
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
